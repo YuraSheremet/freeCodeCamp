ffcf5156c508cd2b9f0ec2f7069400b133bb65bf
Yurii <sheremet.yu@gmail.com>
2019-05-21 18:25:24 +0300

    new file

---
+++ Basic Algorithm Scripting/TitleCaseASentence.js
@@ -0,0 +1,8 @@
+function titleCase(str) {
+    let result = str.toLowerCase().split(' ').map(function(item) {
+        return item[0].toUpperCase() + item.slice(1);
+    })
+    return result;
+}
+
+titleCase();
