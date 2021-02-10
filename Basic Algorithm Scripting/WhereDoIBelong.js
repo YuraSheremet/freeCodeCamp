8c848f47dd01fd4f414c9c4efe99af7aa870288d
Yurii <sheremet.yu@gmail.com>
2021-02-10 19:42:31 +0200

    commit

--- Basic Algorithm Scripting/WhereDoIBelong.js
+++ Basic Algorithm Scripting/WhereDoIBelong.js
@@ -1,9 +1,11 @@
 function getIndexToIns(arr, num) {
-    arr.push(num);
-    arr.sort(function(a, b) {
-      return a - b;
-    });
-    return arr.indexOf(num);
+  arr.push(num);
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  return arr.indexOf(num);
 }
   
 getIndexToIns([40, 60], 50);
+
+//to be continue 
